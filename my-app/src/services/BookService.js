a17d7c547a2c02a5203e041c93140a097a95ceaa
agnes0304 <jiwoochoi0304@gmail.com>
2023-08-02 13:41:20 +0900

    api2 done

--- my-app/src/services/BookService.js
+++ my-app/src/services/BookService.js
@@ -13,3 +13,18 @@ export const createBook = async (data) => {
       })
     return await response.json();
 }
+
+export const getAllTodos = async () => {
+
+    const response = await fetch('/api2/todos');
+    return await response.json();
+}
+
+export const createTodo = async (data) => {
+    const response = await fetch('/api2/todo', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({book: data})
+      })
+    return await response.json();
+}
